d0697c3518ddd2268aa92996865bec40c0752a14
AshishKhatiwada1 <ashkhatnp@gmail.com>
2018-09-20 15:36:24 +0545

    first commit

---
+++ jbNew/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace jbNew
+{
+    public class Class1
+    {
+        public string ctdb(string s)
+        {
+            string a = "'";
+            string b = s;
+            b = a + b;
+            b = b + a;
+            return b;
+        }
+    }
+}
